13709628729ba33d9048008498cba373d60009d3
angelo <aurtula1@gmail.com>
2022-01-24 00:53:15 -0500

    commit tree d6469081e4e95627095dec02b58ab3025d5ae457

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
 // write your createStore function here
-
+function createStore(reducer){
+let state;
+  function dispatch(action){
+    state = reducer(state, action);
+    render()
+  };
+  function getState(){
+    return state
+  }
+  return {dispatch, getState}
+}
 function candyReducer(state = [], action) {
   switch (action.type) {
     case 'ADD_CANDY':
@@ -8,7 +18,8 @@ function candyReducer(state = [], action) {
       return state;
   }
 }
-
+let store = createStore(candyReducer)
+store.dispatch({ type: '@@INIT' });
 function render() {
   let container = document.getElementById('container');
   if(store.getState()) {
